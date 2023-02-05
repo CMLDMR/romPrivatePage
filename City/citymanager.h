b9405e30506c5e4153d5c5f4d5e635941ee0c108
Cemal DEMIR <cemaldemir07@gmail.com>
2023-02-05 12:33:53 +0300

    Şehir Control Widget Eklendi.

---
+++ City/citymanager.h
@@ -0,0 +1,16 @@
+#ifndef CITY_CITYMANAGER_H
+#define CITY_CITYMANAGER_H
+
+#include <Wt/WContainerWidget.h>
+
+namespace City {
+
+class CityManager : public Wt::WContainerWidget
+{
+public:
+    CityManager();
+};
+
+} // namespace City
+
+#endif // CITY_CITYMANAGER_H
